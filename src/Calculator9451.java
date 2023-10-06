6343eccf084324c63222764aa36faeebf20c9911
unknown <f219451@NUCFD.EDU.LOCAL>
2023-10-06 10:51:58 +0500

    Created calcultor by 21F-9451

---
+++ src/Calculator9451.java
@@ -0,0 +1,15 @@
+
+public class Calculator9451 {
+
+	public static void main(String[] args) {
+		int v1=16;
+		int v2=15;
+		
+		int sum=v1+v2;
+		System.out.println("Addition:"+sum);
+		sum=v1-v2;
+		System.out.println("Subtraction:"+sum);
+
+	}
+
+}
