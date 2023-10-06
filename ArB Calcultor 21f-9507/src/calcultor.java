e72e3efb8ca447560a06049ac786182179cd4015
AbdulRehman-9876 <abdulrehman9876@gmail.com>
2023-10-06 10:35:30 +0500

    Added Multiplication

--- ArB Calcultor 21f-9507/src/calcultor.java
+++ ArB Calcultor 21f-9507/src/calcultor.java
@@ -10,12 +10,17 @@ public class calcultor {
 	{
 		return d - e;
 	}
+	double mult(double a,double b)
+	{
+		return a * b;
+	}
 	
 	public static void main(String[] args) {
 		
 		calcultor obj = new calcultor();
 		System.out.println(obj.add(10.2,21.2));
 		System.out.println((obj.sub(20.4, 10.2)));
+		System.out.println((obj.mult(2.4, 1.22)));
 
 }
 }
